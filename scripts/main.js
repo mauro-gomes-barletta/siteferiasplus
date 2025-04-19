92d2b33114a42e391bd544f16f566a0b257d92df
mauro-gomes-barletta <mauro.barletta.mb@gmail.com>
2025-04-19 16:48:54 -0300

    ajuste

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 document.getElementById('root').innerHTML = `
-    <main style="text-align: center;">
-        <img src="./styles/assets/logo.png" alt="Logotipo FériasPlus" class="logo" style="display: block; margin: 0 auto; height: 200px;">
+    <main>
         <h2>Planeje suas férias de forma inteligente!</h2>
         <p>Escolha os melhores períodos, destinos e atividades para aproveitar ao máximo.</p>
     </main>
